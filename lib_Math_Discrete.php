2c191f0d5b006105a12a49951e792ee326886869
InExSu <InExSu@bk.ru>
2023-01-05 21:13:49 +0300

    handLocal

--- lib_Math_Discrete.php
+++ lib_Math_Discrete.php
@@ -6,28 +6,48 @@
  * находит подмножество ребер, которое образует дерево, включающее каждую вершину,
  * где общий вес всех ребер в дереве минимизирован.
  * алгоритм поиска минимального остовного дерева (minimum spanning tree, MST
+ * НЕ путай с кратчайшим путём
  */
 function graph_Prim(array $arr_Graph): array {
 
-    $arr_Edges = [];
-    $arr_Way   = [];
+    $arr_Way = [];
 
     $arr_Graph_Count = count($arr_Graph);
 
     for ($i = 0; $i < $arr_Graph_Count; $i++) {
-        $arr_Edges[] = graph_Vertex_Edges($arr_Graph, $arr_Graph);
-        $arr_Way[]   = graph_Edges_Min();
+
+        $arr_Edges = graph_Vertex_Edges($arr_Graph, $arr_Graph[$i][0]);
+
+        if (count($arr_Edges) > 0)
+            $arr_Way[] = graph_Edges_Min($arr_Edges);
     }
 
     return $arr_Way;
-
 }
 
 /**
- * для алгоритма Прима - для вершины найти реобро
+ * массив рёбер вершины
  */
 function graph_Vertex_Edges(array $arr_Graph, string $vertex): array {
 
+    static $arr_Vertex = [];
+
+    $arr_Edges = [];
+
+    if (!isset($arr_Vertex[$vertex])) {
+
+        $arr_Vertex[$vertex] = 0;
+
+        foreach ($arr_Graph as $arr1d) {
+
+            // вторая вершина не должа быть в обработанных
+            if (!isset($arr_Vertex[$arr1d[1]]))
+
+                if ($arr1d[0] == $vertex)
+                    $arr_Edges[] = $arr1d;
+        }
+    }
+    return $arr_Edges;
 }
 
 /**
@@ -35,4 +55,17 @@ function graph_Vertex_Edges(array $arr_Graph, string $vertex): array {
  */
 function graph_Edges_Min(array $arr_Edges): array {
 
+    $arr_Edges_Count = count($arr_Edges);
+    $edge_Min        = PHP_INT_MAX;
+    $index_Min       = PHP_INT_MAX;
+
+    for ($i = 0; $i < $arr_Edges_Count; $i++) {
+
+        if ($arr_Edges[$i][2] < $edge_Min) {
+
+            $edge_Min  = $arr_Edges[$i][2];
+            $index_Min = $i;
+        }
+    }
+    return $arr_Edges[$index_Min] ?? [];
 }
--- lib_Math_Discrete_Tests.php
+++ lib_Math_Discrete_Tests.php
@@ -2,12 +2,10 @@
 
 require_once 'lib_Math_Discrete.php';
 
-/**
- * алгоритм Прима (также известный как алгоритм Ярника) представляет собой жадный алгоритм, который находит минимальное связующее дерево для взвешенного неориентированного графа. Это означает, что он находит подмножество ребер, которое образует дерево, включающее каждую вершину, где общий вес всех ребер в дереве минимизирован.
- */
-function graph_Prim_Test() {
-// граф на рисунке 1.6
-    $arr_Graph = [
+function array_Graph_1_6(): array {
+    // формула Excel ="['"& A1 & "', '" & B1 & "', " & C1 & "],"
+    // граф на рисунке 1.6
+    return [
         ['A', 'B', 3],
         ['A', 'F', 2],
         ['B', 'C', 3],
@@ -20,10 +18,48 @@ function graph_Prim_Test() {
         ['E', 'G', 3],
         ['E', 'F', 4],
         ['F', 'G', 3],
+        ['G', 'B', 6],
+        ['G', 'C', 1],
+        ['G', 'E', 4],
+        ['G', 'F', 3],
     ];
+}
+
+/**
+ * алгоритм Прима (также известный как алгоритм Ярника) представляет собой жадный алгоритм, который находит минимальное связующее дерево для взвешенного неориентированного графа. Это означает, что он находит подмножество ребер, которое образует дерево, включающее каждую вершину, где общий вес всех ребер в дереве минимизирован.
+ */
+function graph_Prim_Test() {
+
+    $arr_Graph = array_Graph_1_6();
 
     $arr_Way = graph_Prim($arr_Graph);
     assert(is_array($arr_Way));
 }
 
+function graph_Vertex_Edges_Test() {
+    echo 'graph_Vertex_Edges_Test($arr_Graph,$vertex)';
+    $arr_Graph = array_Graph_1_6();
+    $vertex    = 'A';
+    $arr_A     = [
+        ['A', 'B', 3],
+        ['A', 'F', 2]];
+    $arr_Diff  = [];
+
+    $arr_Edges = graph_Vertex_Edges($arr_Graph, $vertex);
+    $arr_Diff  = array_diff($arr_Graph, $arr_Edges);
+    assert(count($arr_Diff) == 0);
+}
+
+function graph_Edges_Min_Test() {
+    echo 'graph_Edges_Min_Test()';
+    $arr_A = [
+        ['A', 'B', 3],
+        ['A', 'F', 2]];
+
+    $arr = graph_Edges_Min($arr_A);
+    assert($arr[2] == 2);
+}
+
+// graph_Edges_Min_Test();
+// graph_Vertex_Edges_Test();
 graph_Prim_Test();
